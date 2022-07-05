32468b76695c2b0af240f7d0314465522db9c8cd
alecsandrvarava <alecsandrvarava@ukr.net>
2022-07-05 15:26:38 +0300

    header

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,10 +1,25 @@
-const items = document.querySelectorAll(".header__list-item");
-items.forEach((el) => {
-  el.addEventListener("click", (e) => {
-    if (e.target.classList.contains("dropdown")) {
-      let self = e.currentTarget;
-      let item = self.querySelector(".header__sub-list");
-      item.classList.toggle("hidden");
+document.addEventListener("click", (e) => {
+  let self = e.target;
+  if (self.closest(".header__list-item")) {
+    let parent = self.closest(".header__list-item");
+    if (parent.classList.contains("dropdown")) {
+      let item = parent.querySelector(".header__sub-list");
+      if (item.classList.contains("hidden")) {
+        const headerSubList = document.querySelectorAll(".header__sub-list");
+        headerSubList.forEach((el) => {
+          el.classList.add("hidden");
+        });
+        item.classList.remove("hidden");
+      } else {
+        item.classList.add("hidden");
+      }
     }
-  });
+  } else if (!self.closest(".header__list-item")) {
+    const headerSubList = document.querySelectorAll(".header__sub-list");
+    headerSubList.forEach((el) => {
+      if (!el.classList.contains("hidden")) {
+        el.classList.add("hidden");
+      }
+    });
+  }
 });

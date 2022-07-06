89f2f408167e839402959c6d12d62eea3eeb4113
alecsandrvarava <alecsandrvarava@ukr.net>
2022-07-07 02:07:42 +0300

    arr

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -23,3 +23,17 @@ document.addEventListener("click", (e) => {
     });
   }
 });
+const dropdown = document.querySelectorAll(".dropdown");
+if (dropdown) {
+  dropdown.forEach((element) => {
+    console.log(element);
+    element.insertAdjacentHTML(
+      "beforeend",
+      `<span class="header__arrow">
+					<svg  viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M5.5 9L0.73686 0.75L10.2631 0.75L5.5 9Z" fill="white"/>
+					</svg>
+				</span>`
+    );
+  });
+}
